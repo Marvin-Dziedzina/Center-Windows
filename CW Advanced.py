5cf9be11d5cd64013d69ed3e0389e5594672086d
TheGoldfinder <66122129+TheGoldfinder@users.noreply.github.com>
2021-12-06 17:49:24 +0100

    Autostart Feature
    Added Autostart Feature
    Added Build Configs
    Added Button for Shortcut on Desktop

--- CW Advanced.py
+++ CW Advanced.py
@@ -1,11 +1,18 @@
-import pygetwindow as gw
+from pygetwindow import getAllWindows
 from tkinter import *
-import keyboard
+from keyboard import add_hotkey
 import webbrowser
+from shutil import copy2
+import win32com.client
+import pythoncom
+import os
+import time
 
-# change for build
-# picturePath = ".\\CenterWindow\\icon\\" # the relative path on my computer
-picturePath = ".\\icon\\"  # the relative path if in the build is an folder
+programPath = os.path.dirname(os.path.realpath(__file__))
+picturePath = programPath + "\\icon\\"
+shortcutName = "CW startup.lnk"
+startupPath = "C:\\Users\\" + os.getlogin() + \
+    "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
 
 
 # main window
@@ -71,12 +78,12 @@ class App(Tk):
         t.place(relx=1.0, rely=1.0, anchor="se")
 
         # hotkey for fast centering
-        keyboard.add_hotkey("ctrl+alt+<", self.centerWindows)
+        add_hotkey("ctrl+alt+<", self.centerWindows)
 
     # function to center all windows except all maximized or minimized windows
     def centerWindows(self):
         # get all windows
-        self.windows = gw.getAllWindows()
+        self.windows = getAllWindows()
 
         # check each window if not maximised or minimized and if name is not emty and if the window not self
         for i in self.windows:
@@ -97,7 +104,7 @@ class App(Tk):
     # centeres it self
     def centerSelf(self):
         # get all windows
-        self.windows = gw.getAllWindows()
+        self.windows = getAllWindows()
 
         for i in self.windows:
             if i.title == "CW Advanced":
@@ -115,8 +122,15 @@ class App(Tk):
 
 # second window
 class AboutUs(Tk):
+    autostart = ""
+    startTime = 0
+
     def __init__(self):
         super().__init__()
+
+        self.startTime = time.time()
+        self.autostart = BooleanVar()
+
         # window is defined
         self.geometry("300x400")
         self.title("CW Advanced")
@@ -125,7 +139,7 @@ class AboutUs(Tk):
         self.config(background="#242529")
 
         # elements in the second window are defined under here
-        t = Label(self, text="Special thanks to Moritz")
+        t = Label(self, text="Special thanks to Moritz\naka Brat__Wurst")
         t.config(font=("Arial", 16), background="#242529", foreground="#a6abb3")
         t.place(relx=0.5, rely=0.03, anchor="n")
 
@@ -135,12 +149,75 @@ class AboutUs(Tk):
                  activebackground="#292c30", foreground="#a6abb3")
         t.place(relx=0.5, rely=0.7, anchor="center")
 
+        t = Button(self, text="Desktop Shortcut", width=14,
+                   height=1, command=self.shortcutToDesktop)
+        t.config(font=("Arial", 8), background="#373a40",
+                 activebackground="#292c30", foreground="#a6abb3")
+        t.place(relx=0.0, rely=1.0, anchor="sw")
+
+        checkBox = Checkbutton(self, text="Autostart", variable=self.autostart,
+                               background="#242529", foreground="#a6abb3")
+        checkBox.place(relx=0.5, rely=1.0, anchor="s")
+
+        self.createShortcut()
+
+        # Path to startup folder
+        if os.path.isfile(startupPath + "\\" + shortcutName):
+            checkBox.select()
+
+        self.protocol("WM_DELETE_WINDOW", self.onClose)
+
+    # Check waht the user want and where the program is
+    def onClose(self):
+        if os.path.isfile(startupPath + "\\" + shortcutName) and self.autostart.get():
+            self.destroy()
+        elif not os.path.isfile(startupPath + "\\" + shortcutName) and not self.autostart.get():
+            self.destroy()
+        elif os.path.isfile(startupPath + "\\" + shortcutName) and not self.autostart.get():
+            try:
+                os.remove(startupPath + "\\" + shortcutName)
+                self.destroy()
+            except:
+                print("Error can not remove file")
+                self.destroy()
+        elif not os.path.isfile(startupPath + "\\" + shortcutName) and self.autostart.get():
+            try:
+                copy2(shortcutName, startupPath)
+                self.destroy()
+            except:
+                print("Error can not copy and or paste file")
+                self.destroy()
+        else:
+            self.destroy()
+
     # open link
     def discordLink(self):
         webbrowser.open("https://discord.com/invite/pNpyYAhz5U")
+
+    def createShortcut(self):
+        if not os.path.isfile(shortcutName):
+            try:
+                shell = win32com.client.Dispatch("WScript.Shell")
+                shortcut = shell.CreateShortCut(
+                    programPath + "\\" + shortcutName)
+                shortcut.Targetpath = programPath + "\\" + "CW Advanced.exe"
+                shortcut.IconLocation = programPath + "\\icon\\CenterWindow.ico"
+                shortcut.WindowStyle = 1
+                shortcut.save()
+            except:
+                print("Could not create shortcut")
+
+    def shortcutToDesktop(self):
+        if not os.path.isfile(shortcutName):
+            self.createShortcut()
+        copy2(shortcutName, os.path.join(
+            os.path.join(os.environ["USERPROFILE"]), "Desktop"))
 
 
 # start mainloop
 if __name__ == "__main__":
     App().mainloop()
     AboutUs().mainloop()
+
+
+# os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
